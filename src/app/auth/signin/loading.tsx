1b19b550d29d4d32a9ee1d8804a06409a792380b
Houman <hwendev@gmail.com>
2025-06-01 22:34:59 +0800

    refactor: remove loading components for sign-in and verify request

--- src/app/auth/signin/loading.tsx
+++
@@ -1,7 +0,0 @@
-import { LoadingSpinner } from "~/components/ui/loading-spinner";
-
-export default function SignInLoading() {
-  return (
-    <LoadingSpinner fullScreen text="Checking authentication..." size="lg" />
-  );
-}
--- src/app/auth/verify-request/loading.tsx
+++
@@ -1,7 +0,0 @@
-import { LoadingSpinner } from "~/components/ui/loading-spinner";
-
-export default function VerifyRequestLoading() {
-  return (
-    <LoadingSpinner fullScreen text="Checking authentication..." size="lg" />
-  );
-}
