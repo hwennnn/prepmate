78e67146e937903d849f69bfd9ab94bc791129ec
Houman <hwendev@gmail.com>
2025-06-08 21:47:04 +0800

    fix: only build production when its merging to main
    (ouch man, vercel can't configure something easy like showing "preview" deployment only when a PR is created ://)

--- ignore-vercel-build.sh
+++ ignore-vercel-build.sh
@@ -3,7 +3,7 @@
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 echo "VERCEL_ENV: $VERCEL_ENV"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_ENV" == "preview" ]]; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]]; then
   echo "✅ - Build can proceed"
   exit 1
 else
