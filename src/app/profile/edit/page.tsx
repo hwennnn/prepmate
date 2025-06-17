85ea0538e8c66a29978915f99628e61510cd63f3
eejon <eejonlee@gmail.com>
2025-06-18 00:26:10 +0800

    refactor: clean up code for uneccessary imports - Removed the error boundary wrapper (no longer manually implemented)

--- src/app/profile/edit/page.tsx
+++ src/app/profile/edit/page.tsx
@@ -1,13 +1,10 @@
 import { OnboardingCheck } from "~/app/_components/OnboardingCheck";
 import { EditProfilePageClient } from "~/app/profile/edit/_components/EditProfilePageClient";
-import { ErrorBoundary } from "~/components/ErrorBoundary";
 
 export default function EditProfilePage() {
   return (
     <OnboardingCheck>
-      <ErrorBoundary>
-        <EditProfilePageClient />
-      </ErrorBoundary>
+      <EditProfilePageClient />
     </OnboardingCheck>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,20 +1,11 @@
 import { ProfilePageClient } from "~/app/profile/_components/ProfilePageClient";
 import { OnboardingCheck } from "../_components/OnboardingCheck";
-import { ErrorBoundary } from "~/components/ErrorBoundary";
 
 export default async function ProfilePage() {
-  //const profilePageClientErrorTitle = "Failed to Load Profile";
-  //const profilePageClientErrorMessage = "We couldn't load your profile information. This might be because your profile hasn't been set up yet or there was a network issue.";
 
   return (
     <OnboardingCheck>
-      <ErrorBoundary
-      //title={profilePageClientErrorTitle}
-      //description={profilePageClientErrorMessage}
-      // Still trying to figure out how to better customize error boundary's error details dynamically
-      >
         <ProfilePageClient />
-      </ErrorBoundary>
     </OnboardingCheck>
   );
 }
