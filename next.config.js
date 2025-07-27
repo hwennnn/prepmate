da1c0e2aba1f1ee1eff4fbb12908f30f6c21aa70
Houman <hwendev@gmail.com>
2025-07-27 19:32:52 +0800

    fix: edit resume loading (#39)
    * refactor: remove loading spinner from TemplateSwitcher component
    
    * feat: include WASM files for typst compilation in tRPC API routes

--- next.config.js
+++ next.config.js
@@ -9,6 +9,13 @@ const config = {
   experimental: {
     serverComponentsExternalPackages: ["pdf-parse"],
   },
+  outputFileTracingIncludes: {
+    // Include WASM files for typst compilation in tRPC API routes
+    "/api/trpc/[...trpc]": [
+      "./node_modules/@myriaddreamin/typst-ts-web-compiler/pkg/*.wasm",
+      "./node_modules/@myriaddreamin/typst-ts-renderer/pkg/*.wasm",
+    ],
+  },
 };
 
 export default config;
---
+++ src/app/resume/builder/loading.tsx
@@ -0,0 +1,7 @@
+import { LoadingSpinner } from "~/components/ui/loading-spinner";
+
+export default function ResumeBuilderLoading() {
+  return (
+    <LoadingSpinner fullScreen text="Loading resume builder..." size="lg" />
+  );
+}
